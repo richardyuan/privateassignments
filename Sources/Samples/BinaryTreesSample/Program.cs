71a354ad03b9dab95ef3c7295c2605a29b9cb68f
Richard <richardyuan_@hotmail.com>
2016-11-20 10:41:37 +0100

    assignment

---
+++ Sources/Samples/BinaryTreesSample/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreesSample
+{
+  interface ITree<T>
+  {
+    bool IsEmpty { get; }
+    T Value { get; }
+    ITree<T> Left { get; }
+    ITree<T> Right { get; }
+  }
+
+  class Empty<T> : ITree<T>
+  {
+    public bool IsEmpty
+    {
+      get
+      {
+        return true;
+      }
+    }
+
+    public ITree<T> Left
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public ITree<T> Right
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public T Value
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+  }
+
+  class Node<T> : ITree<T>
+  {
+    public bool IsEmpty
+    {
+      get
+      {
+        return false;
+      }
+    }
+
+    public ITree<T> Left { get; set; }
+
+    public ITree<T> Right { get; set; }
+
+    public T Value { get; set; }
+
+    public Node(ITree<T> l, T v, ITree<T> r)
+    {
+      Value = v;
+      Left = l;
+      Right = r;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var t = new Empty<int>() as ITree<int>;
+      t = Insert(t, 50);
+      t = Insert(t, 25);
+      t = Insert(t, 100);
+      t = Insert(t, 666);
+      t = Insert(t, 12);
+      t = Insert(t, 80085);
+      //PrintInOrder(t);
+      Console.WriteLine(SearchElement(t, 68));
+    }
+
+    static ITree<int> Insert(ITree<int> t, int v)
+    {
+      if (t.IsEmpty)
+        return new Node<int>(new Empty<int>(), v, new Empty<int>());
+
+      if (t.Value == v)
+        return t;
+
+      if (v < t.Value)
+        return new Node<int>(Insert(t.Left, v), t.Value, t.Right);
+      else
+        return new Node<int>(t.Left, t.Value, Insert(t.Right, v));
+    }
+
+    static bool SearchElement(ITree<int> t, int v)
+    {
+      if (t.IsEmpty)
+        return false;
+      else
+      {
+        Console.WriteLine("Looking in " + t.Value);
+        if (t.Value == v)
+          return true;
+        else
+        {
+          if (v < t.Value)
+            return SearchElement(t.Left, v);
+          else
+            return SearchElement(t.Right, v);
+        }
+      }
+
+    }
+
+    static void PrintPreOrder<T>(ITree<T> t)
+    {
+      if (t.IsEmpty) return;
+      Console.WriteLine(t.Value);
+      PrintPreOrder(t.Left);
+      PrintPreOrder(t.Right);
+    }
+
+    static void PrintInOrder<T>(ITree<T> t)
+    {
+      if (t.IsEmpty) return;
+      PrintInOrder(t.Left);
+      Console.WriteLine(t.Value);
+      PrintInOrder(t.Right);
+    }
+
+  }
+}
---
+++ Sources/Samples/GraphExploration/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExploration
+{
+  enum ExplorationStatus
+  {
+    Explored,
+    Unexplored,
+    Frontier
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var V = new List<string> { "A", "B", "C", "D", "E", "F", "G" };
+
+      List<int>[] E =
+      {
+        new List<int>() { 1, 2, 3 },
+        new List<int>() { 2, 3 },
+        new List<int>() { 1, 3 },
+        new List<int>() { 0, 1, 2, 4, 5, 6 },
+        new List<int>() {},
+        new List<int>() {},
+        new List<int>() {},
+      };
+
+      BFS(V, E);
+
+    }
+
+    private static void XFS(List<string> V, List<int>[] E,
+      Action<int> addToFrontier, Func<bool> isFrontierEmpty, Func<int> getNextFromFrontier)
+    {
+      var status =
+        (from v in Enumerable.Range(0, V.Count)
+         select ExplorationStatus.Unexplored).ToArray();
+
+      addToFrontier(0);
+      status[0] = ExplorationStatus.Frontier;
+
+      while (!isFrontierEmpty())
+      {
+        var v = getNextFromFrontier();
+        Console.WriteLine("Now exploring " + V[v]);
+        Console.ReadLine();
+        status[v] = ExplorationStatus.Explored;
+        foreach (var c in E[v])
+        {
+          if (status[c] == ExplorationStatus.Unexplored)
+          {
+            addToFrontier(c);
+            status[c] = ExplorationStatus.Frontier;
+          }
+        }
+      }
+    }
+
+    private static void DFS(List<string> V, List<int>[] E)
+    {
+      var frontier = new Stack<int>();
+      XFS(V, E, v => frontier.Push(v), () => frontier.Count == 0, () => frontier.Pop());
+    }
+
+    private static void BFS(List<string> V, List<int>[] E)
+    {
+      var frontier = new Queue<int>();
+      XFS(V, E, v => frontier.Enqueue(v), () => frontier.Count == 0, () => frontier.Dequeue());
+    }
+  }
+}
