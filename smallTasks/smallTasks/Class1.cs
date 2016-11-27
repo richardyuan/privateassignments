e332d3acd241312be5bd5f15ab2f608f8821b5ef
Richard <richardyuan_@hotmail.com>
2016-11-27 14:11:04 +0100

    oijoj

--- smallTasks/smallTasks/Class1.cs
+++ smallTasks/smallTasks/Class1.cs
@@ -8,10 +8,110 @@ namespace smallTasks
 {
     class Class1
     {
-        public static int[] cardDeck = { 5, 2, 4, 6, 1, 3 };
-        static int deckLength = cardDeck.Length;
+        public static int[] cardDeck = { 5, 4, 3, 1, 2, 6 };
         
+        public static void sortMerge(int[] list, int low, int high)
+        {
+            if (high > low)
+            {
+                int middle = (high + low) / 2;
+                sortMerge(list, low, high);
+                sortMerge(list, (middle + 1), high);
+                merge(list, low, middle, high);
+            }
+        }
+
+        public static void merge(int[] list, int low, int middle, int high)
+        {
+            int L = low;
+            int R = middle + 1;
+            int[] temp = new int[(high - low) + 1];
+            int tempIndex = 0;
+
+            while ((L <= middle) && (R <= high))
+            {
+                if (list[L] < list[R])
+                {
+                    temp[tempIndex] = list[L];
+                    L = L + 1;
+                }
+                else
+                {
+                    temp[tempIndex] = list[R];
+                    R = R + 1;
+                }
+                tempIndex = tempIndex + 1;
+            }
+
+            if (L <= middle)
+            {
+                while (L <= middle)
+                {
+                    temp[tempIndex] = list[L];
+                    L = L + 1;
+                    tempIndex = tempIndex + 1;
+                }
+            }
+
+            if (R <= high)
+            {
+                while (R <= high)
+                {
+                    temp[tempIndex] = list[R];
+                    R = R + 1;
+                    tempIndex = tempIndex + 1;
+                }
+            }
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                list[low + i] = temp[i];
+            }
+           
+        }
+
+
         
+        public static int[] insertionSort(int[] list)
+        {
+            for (int i = 1; i < list.Length; i++)
+            {
+                int key = list[i];
+                int current = i - 1;
+
+                while (current >= 0 && key < list[current])
+                {
+                    list[current + 1] = list[current];
+                    current = current - 1;
+                }
+                list[current + 1] = key;
+            }
+            return list;
+        }
+
+
+        public static int[] mergeSort(int[] list)
+        {
+
+
+
+            return list;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The length of the deck is: " + cardDeck.Length);
+            Console.WriteLine("Values before the algorithm: ");
+            getData(cardDeck);
+            insertionSort(cardDeck);
+            Console.WriteLine("After the algorithm: ");
+            getData(cardDeck);
+
+            Console.ReadKey();
+        }
+
         public static void getData(int[] list)
         {
             foreach (int item in list)
@@ -20,53 +120,5 @@ namespace smallTasks
             }
         }
 
-        public static int[] insertionAlgorithm(int[] list)
-        {
-            for (int i = 1; i < list.Length; i++)
-            {
-                int key = list[i];
-                int j = i - 1;
-
-                while (j >= 0 && key < list[j])
-                {
-                    list[j + 1] = list[j];
-                    int temp = list[j];
-                    j = j - 1;
-                }
-                list[j + 1] = key;
-            }
-            return list;
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("The length of the deck is: " + deckLength);
-            Console.WriteLine("Values before the algorithm: ");
-            getData(cardDeck);
-            insertionAlgorithm(cardDeck);
-            Console.WriteLine("After the algorithm: ");
-            getData(cardDeck);
-
-            Console.ReadKey();
-
-            
-        }
-
-        public static void insertionSort(int[] unsortedDeck)
-        {
-            for (int index = 0; index < deckLength - 1; index++)
-            {
-                int key = unsortedDeck[index];
-                int prevIndex = index - 1;
-
-                while (prevIndex >= 0 && unsortedDeck[prevIndex] > key)
-                {
-                    unsortedDeck[prevIndex + 1] = unsortedDeck[prevIndex];
-                    prevIndex = prevIndex - 1;
-                }
-                unsortedDeck[prevIndex + 1] = key;
-            }
-            Console.WriteLine("done");
-        }
     }
 }
